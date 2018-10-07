0430da3c6b8de59127a4259258ea7c51b71b80a9
Zaglyada Ivan <ZVanoFD18@gmail.com>
2018-10-07 19:16:58 +0300

    Исправлен двойной обработчик клика по пункту вложенного меню.

--- js/task01.js
+++ js/task01.js
@@ -1,17 +1,12 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", function () {
     let elsMenu = document.getElementsByClassName('menu');
-    // for (let i=0; i < elsMenu.length; i++){
-    //     let elMenu = elsMenu[i];
-    // }
     [].forEach.call(elsMenu, function (elMenu) {
         let elMenuText = elMenu.querySelector('.menu-text'),
             elMenuItems = elMenu.querySelector('.menu-items');
         elMenuText.addEventListener('click', function (event) {
-            console.log('elMenuText/click', arguments);
+            console.log('.menu-text/click', arguments);
             event.stopPropagation();
-            // let elMenuText = event.target.parentElement.querySelector('.menu-text'),
-            //     elMenuItems = event.target.parentElement.querySelector('.menu-items');
             if (elMenuItems.classList.contains('hidden')) {
                 elMenuItems.classList.remove('hidden')
             } else {
@@ -22,8 +17,9 @@ document.addEventListener("DOMContentLoaded", function () {
         let elsMenuItem = elMenu.getElementsByClassName('menu-item');
         [].forEach.call(elsMenuItem, function (elMenuItem) {
             elMenuItem.addEventListener('click', function (event) {
-                console.log('menu-item/click', arguments);
-                event.stopPropagation();
+                console.log('.menu-item/click', arguments);
+                alert('Нажали на пункт меню [' + event.target.innerText + ']')
+                event.stopImmediatePropagation();
             });
         });
     });
